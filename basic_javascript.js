5ef86ab00f14e877a42298a309306750607631e7
YahyaAdityaS <putrayayah47@gmail.com>
2024-08-07 11:12:25 +0700

    test basic java script

---
+++ basic_javascript.js
@@ -0,0 +1,9 @@
+// variabel
+let nama= "Noline" //String
+let usia= 16       //int
+let single = true  //boolean
+
+//constanta
+let pi = 3.14      //float/double
+
+console.log(nama + " Berumur " + usia + " Tahun") 
